2657aaa21c5a868ea11179d6a39707febeaf12b4
yuanhai <void@yuanhais-MacBook-Pro.local>
2016-07-29 13:53:00 +0800

    simple demo

--- src/ccdd4ever.com/main/app/app.go
+++ src/ccdd4ever.com/main/app/app.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"html/template"
+	"regexp"
 )
 
 func sayHello(res http.ResponseWriter,req *http.Request){
@@ -21,9 +23,36 @@ func sayHello(res http.ResponseWriter,req *http.Request){
 
 
 }
+
+func login(res http.ResponseWriter,req *http.Request) {
+	fmt.Println("method:",req.Method)
+	if req.Method=="GET" {
+		t,_:=template.ParseFiles("front/src/templates/login.gtpl")
+		res.Header().Set("Content-Type", "text/html; charset=utf-8")
+		t.Execute(res,nil)
+	}else {
+		req.ParseForm()//显式调用解析表单数据
+		fmt.Println("username:",req.Form["username"])
+		fmt.Println("password:",req.Form["password"])
+		//验证表单输入
+		//非空
+		if len(req.Form["username"][0])==0{
+			fmt.Fprint(res,"username must be input")
+		}
+		//正则判断
+		if m,_:=regexp.MatchString(("^[0-9]+$"),req.Form.Get("password"));!m{
+			fmt.Fprint(res,"invalid password")
+		}else {
+			fmt.Fprint(res,"welcome")
+		}
+	}
+	
+}
+
 func main() {
-	http.HandleFunc("/",sayHello)
-	err :=http.ListenAndServe(":9090",nil)
+	http.HandleFunc("/",sayHello)//绑定请求路径
+	http.HandleFunc("/login",login)
+	err :=http.ListenAndServe(":9090",nil)//监听端口
 	if err !=nil{
 		log.Fatalf("ListenAndServe",err)
 	}
